92eb6dc852d6989c43bb0a863cea495b5d3039c1
E224733A <noebracq973@gmail.com>
2026-06-10 14:57:49 +0200

    docs: commenter le controller home

--- src/MobileSLI.Expedition.Web/Controllers/HomeController.cs
+++ src/MobileSLI.Expedition.Web/Controllers/HomeController.cs
@@ -2,6 +2,10 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MobileSLI.Expedition.Web.Controllers;
 
+/// <summary>
+/// Contrôleur minimal conservé pour la page d'erreur MVC standard.
+/// Les routes métier Expédition, Administration et Verrouillage sont portées par leurs contrôleurs dédiés.
+/// </summary>
 public sealed class HomeController : Controller
 {
     [HttpGet("/Home/Error")]
